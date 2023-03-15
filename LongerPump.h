c74aaf2cadef72dba15987cfd70a283aa0cd8a5f
Philipyeo31 <philipyeo31@gmail.com>
2023-03-15 06:13:52 +0000

    Update file LongerPump.h

--- LongerPump.h
+++ LongerPump.h
@@ -1,3 +1,5 @@
+//byte example[] = {EF, addr, len, W, J, speed1stbyte, speed2ndbyte, full/normal speed and start/stop, dir, xor};
+// example[] ={0xE9, 0x01, 0x06, 0x57, 0x4A, 0x01, 0xF4, 0x01, 0x01, 0xEF}; 
 byte example[] = {233, 1, 6, 87, 74, 1, 244, 1, 1, 239}; //example in dec (base10)
 byte sendToPump[] = {};
 byte exampleStop[] = {0xE9, 0x01, 0x06, 0x57, 0x4A, 0x01, 0xF4, 0x00, 0x01, 0xEE};
@@ -5,9 +7,11 @@ byte pumpStop[] = {};
 byte receivedByte[] = {};
 
 
-void run(int address, int speed /*RPM in units of 0.1. e.g 60.0RPM is 600*/, float revolutions /*0 will make pump run indefinitely*/, bool direction){
+void run(int address, int speed /*RPM in units of 0.1. e.g 60.0RPM is 600*/, float revolutions, bool direction){
   byte length = 0x6; //lenth of pdu for sending instructions always 6 bytes
-  
+  bool sentFlag = false; //flag to signal command has been sent
+  bool stopFlag = false; //flag to signal STOP command has been sent
+
   //run duration calculation
   unsigned long runDurationms = 0;
   runDurationms = revolutions / speed * 600000; //6,000 instead of 60,000 is because rpm is in resolution of .1
@@ -57,36 +61,50 @@ void run(int address, int speed /*RPM in units of 0.1. e.g 60.0RPM is 600*/, flo
   sendToPump[9] = xorCheck; //XOR of address, length and pdu
 
   //send instructions to pump
-  for (int i=0; i<=9; i++)
-    Serial.write(sendToPump[i]);  
+
+  if (sentFlag = false){
+    for (int i=0; i<=9; i++){
+      Serial.write(sendToPump[i]);
+      if (i==9){
+        sentFlag = true;
+      }
+    }
+  }
+
+  pumpStop[0] = 233; //start flag
+  pumpStop[1] = address; //Address for pump
+  pumpStop[2] = 6; // instructions to the pump (PDU) to run is always 6 byte long
+  pumpStop[3] = 8 7; //W ascii HEX code but in base 10 INT
+  pumpStop[4] = 74; //J ascii HEX code but in base 10 INT
+  pumpStop[5] = speed_first_byte; //speed first byte
+  pumpStop[6] = speed_second_byte; //speed second byte
+  pumpStop[7] = 0; //STOP
+  pumpStop[8] = direction; //1 = Clockwise , 0 = Counter-Clockwise
+  pumpStop[9] = xorCheckStop; //XOR of address, length and pdu
 
 
   if (revolutions > 0){
     //stop instructions
-    pumpStop[0] = 233; //start flag
-    pumpStop[1] = address; //Address for pump
-    pumpStop[2] = 6; // instructions to the pump (PDU) to run is always 6 byte long
-    pumpStop[3] = 87; //W ascii HEX code but in base 10 INT
-    pumpStop[4] = 74; //J ascii HEX code but in base 10 INT
-    pumpStop[5] = speed_first_byte; //speed first byte
-    pumpStop[6] = speed_second_byte; //speed second byte
-    pumpStop[7] = 0; //STOP
-    pumpStop[8] = direction; //1 = Clockwise , 0 = Counter-Clockwise
-    pumpStop[9] = xorCheckStop; //XOR of address, length and pdu
+
 
     delay(runDurationms);
 
     //send code to pump
-    for (int i=0; i<=9; i++){
-      Serial.write(pumpStop[i]);
+    if (stopFlag == false){
+      for (int i=0; i<=9; i++){
+        Serial.write(pumpStop[i]);
+        if (i==9){
+        stopFlag = true;
+        }
+      }
     }
   }
-  
-  if (Serial.available() > 0) {
-    byte receivedByte = Serial.read();    
-    Serial.println(receivedByte);
-  }
 }
+
+
+
+
+
 
 
 void stop(int address){
